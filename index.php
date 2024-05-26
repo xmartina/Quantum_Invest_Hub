1b2c2d053fa220cb8b29c7448a27d6e506050803
xmartina <martinsprince126@gmail.com>
2024-05-27 00:22:13 +0100

    d

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
-<?php
-    include_once __DIR__ . ('layout/header.php');
-    dffs
+<?php include_once __DIR__ . ('layout/header.php'); ?>
+
+    
--- layout/header.php
+++ layout/header.php
@@ -1,2 +1,128 @@
-<?php
-    include_once __DIR__ . ('include/general_settins.php');
+<?php include_once __DIR__ . ('include/general_settins.php'); ?>
+
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Investcotrade</title>
+    <!-- favicons Icons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="assets/images/favicons/site.webmanifest" />
+    <meta name="description" content="Aivons HTML Template For Business Consulting" />
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com/">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+        rel="stylesheet">
+
+
+    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/vendors/animate/animate.min.css" />
+    <link rel="stylesheet" href="assets/vendors/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="assets/vendors/jarallax/jarallax.css" />
+    <link rel="stylesheet" href="assets/vendors/jquery-magnific-popup/jquery.magnific-popup.css" />
+    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.min.css" />
+    <link rel="stylesheet" href="assets/vendors/nouislider/nouislider.pips.css" />
+    <link rel="stylesheet" href="assets/vendors/odometer/odometer.min.css" />
+    <link rel="stylesheet" href="assets/vendors/swiper/swiper.min.css" />
+    <link rel="stylesheet" href="assets/vendors/aivons-icons/style.css">
+    <link rel="stylesheet" href="assets/vendors/tiny-slider/tiny-slider.min.css" />
+    <link rel="stylesheet" href="assets/vendors/reey-font/stylesheet.css" />
+    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="assets/vendors/owl-carousel/owl.theme.default.min.css" />
+    <link rel="stylesheet" href="assets/vendors/twentytwenty/twentytwenty.css" />
+    <link rel="stylesheet" href="assets/vendors/bxslider/css/jquery.bxslider.css" />
+    <!-- template styles -->
+    <link rel="stylesheet" href="assets/css/aivons.css" />
+    <link rel="stylesheet" href="assets/css/aivons-responsive.css" />
+
+    <!-- RTL Styles -->
+    <link rel="stylesheet" href="assets/css/aivons-rtl.css">
+
+    <!-- color css -->
+    <link rel="stylesheet" id="jssDefault" href="assets/css/colors/color-default.css">
+    <link rel="stylesheet" id="jssMode" href="assets/css/modes/aivons-normal.css">
+
+    <!-- toolbar css -->
+    <link rel="stylesheet" href="assets/css/aivons-toolbar.css">
+
+    <link rel="stylesheet" type="text/css" href="spop-0.1.3/dist/spop.css">
+    <script src="spop-0.1.3/dist/spop.js"></script>
+
+
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL}, 'google_translate_element');
+}
+</script>
+
+
+
+<script type="text/javascript" src="../../translate.google.com/translate_a/elementa0d8.js?cb=googleTranslateElementInit"></script>
+
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+_smartsupp.key = '6c019f7170e4ad0337de04d2d0f88f8490c1ddb2';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+ 
+
+</head>
+
+
+<body>
+
+
+    <div class="preloader">
+        <div class="preloader__image"></div><!-- /.preloader__image -->
+    </div>
+    <!-- /.preloader -->
+    <div class="page-wrapper">
+        <header class="main-header clearfix">
+            <nav class="main-menu clearfix">
+                <div class="main-menu-wrapper clearfix">
+                    <div class="main-menu-wrapper__left">
+                        <div class="main-menu-wrapper__logo">
+                            <a href="index.html"><img src="assets/images/logo-1.png" alt=""></a>
+                            <div class="nav-search" id="ytWidget"></div><script src="../../translate.yandex.net/website-widget/v1/widget80d6.js?widgetId=ytWidget&amp;pageLang=en&amp;widgetTheme=light&amp;autoMode=false" type="text/javascript"></script>
+                        </div>
+
+
+                        <div class="main-menu-wrapper__main-menu">
+                            <a href="#" class="mobile-nav__toggler">
+                                <span class="mobile-nav__toggler-bar"></span>
+                                <span class="mobile-nav__toggler-bar"></span>
+                                <span class="mobile-nav__toggler-bar"></span>
+                            </a>
+                            <ul class="main-menu__list">
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="indexe47e.html?a=about">About Us</a></li>
+                                <li><a href="index15a0.html?a=support">Contact Us</a></li>
+                                <li><a href="index38cd.html?a=faq">FAQ</a></li>
+                                <li><a href="https://investcotrade.com/L/login">Login</a></li>
+                                <li><a href="https://investcotrade.com/L/register">Create Account</a></li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </div>
+            </nav>
+        </header>
+
+
+
+        <div class="stricky-header stricked-menu main-menu">
+            <div class="sticky-header__content"></div><!-- /.sticky-header__content -->
+        </div><!-- /.stricky-header -->
+
+
